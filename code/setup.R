f26eb77b5a48d753726eded5737d1447b0bd08ae
ClaudiooBarbieri <claudio.barbieri@mail.polimi.it>
2024-12-03 09:13:53 +0100

    i

--- code/setup.R
+++ code/setup.R
@@ -329,7 +329,7 @@ ggplot() +
   geom_sf(data = europe_ue, fill = "lightgray", color = "black") + # Europe map
   geom_sf(data = lucas_2018, aes(colour = '2018'), size = 0.5, alpha = 0.7) + # LUCAS 2018 points
   geom_sf(data = lucas_2015, aes(colour = '2015'), size = 0.5, alpha = 0.7) + # LUCAS 2018 points
-  geom_sf(data = lucas_2018_common, aes(colour = '2015&2018'), size = 0.5, alpha = 0.9) +
+  #geom_sf(data = lucas_2018_common, aes(colour = '2015&2018'), size = 0.5, alpha = 0.9) +
   scale_color_manual(name = "Year", values = c("2018" = "red", "2015" = "green", "2015&2018" = 'purple'), aes(size = 2)) +
   guides(color = guide_legend(override.aes = list(size = 4))) + # Enlarge legend dots
   theme_minimal() +
